81550ae297bf1f7a738c739e8160843d5d174e84
aziuzia <aziuzia@grupazpr.pl>
2023-03-30 19:30:10 +0200

    shallow merge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Alek'
+      name: {firstName: 'Alek', lastName: 'Ziuzia'},
+      company: 'Netstation'
     }
   }
 
@@ -18,11 +19,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi {this.state.name}
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
           </p>
-          <button onClick={ () =>
-            this.setState({name: 'Jan'})
-          }>
+          <button onClick={ () => {
+            this.setState({name: {firstName: 'Jan', lastName: 'Kowalski'}});
+            console.log(this.state);
+          }}>
             Change name
           </button>
         </header>
