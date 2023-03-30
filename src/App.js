40503ed1ec06eccb144361519db7c3fd7b81c34e
aziuzia <aziuzia@grupazpr.pl>
2023-03-30 20:02:00 +0200

    keys for mapping

--- src/App.js
+++ src/App.js
@@ -8,10 +8,22 @@ class App extends Component {
 
     this.state = {
       monsters: [
-        {name: 'Jacek'},
-        {name: 'Placek'},
-        {name: 'Wacek'},
-        {name: 'Maciek'},
+        {
+          name: 'Jacek',
+          id: '1efe21e'
+        },
+        {
+          name: 'Placek',
+          id: '12erwferw'
+        },
+        {
+          name: 'Wacek',
+          id: 'rew1132r'
+        },
+        {
+          name: 'Maciek',
+          id: 'rewqq1223'
+        },
       ]
     }
   }
@@ -21,7 +33,7 @@ class App extends Component {
       <div className="App">
         {
           this.state.monsters.map((monster) => {
-            return <h1>{monster.name}</h1>
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
           })
         }
       </div>
