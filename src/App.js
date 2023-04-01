cd5e8fd5deb3c47cd733d560901c6711812eb930
aziuzia <aziuzia@grupazpr.pl>
2023-04-01 19:32:14 +0200

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
@@ -6,18 +6,26 @@ import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 const App = () => {
+  const [searchField, setSearchField] = useState("");
+  console.log(searchField);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
   return (
     <div className="App">
-        <h1 className="app-title">React Tutorial</h1>
-        {/* <SearchBox
-          className="monster-search-box"
-          placeholder="search monsters"
-          onChangeHandler={onSearchChange}
-        />
-        <CardList monsters={filteredMonsters} /> */}
-      </div>
-  )
-}
+      <h1 className="app-title">React Tutorial</h1>
+      <SearchBox
+        className="monster-search-box"
+        placeholder="search monsters"
+        onChangeHandler={onSearchChange}
+      />
+      {/* <CardList monsters={filteredMonsters} /> */}
+    </div>
+  );
+};
 
 // class App extends Component {
 //   constructor() {
